57fe64f1a75d0e05901d0894c77f9c965c462553
Pallavi-lang <pallaviphadatare24@gmail.com>
2021-01-25 09:55:44 +0530

    first commit

---
+++ src/demojava/PrimativeDataTypeTest.java
@@ -0,0 +1,10 @@
+package demojava;
+
+public class PrimativeDataTypeTest {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("hello666666666");
+	}
+}
